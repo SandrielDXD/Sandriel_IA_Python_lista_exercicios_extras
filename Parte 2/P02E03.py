0b24f8f469511a3ad5aa358cc6bbe3edbae198f4
SandrielDXD <sandriel.sena23@gmail.com>
2025-06-08 19:26:41 -0400

    3

---
+++ Parte 2/P02E03.py
@@ -0,0 +1,10 @@
+numero = float(input("Insira um valor: "))
+
+if numero > 0:
+    print("O valor é Positivo")
+elif numero < 0:
+    print("O valor é Negativo")
+else:
+    print("O valor é Zero (neutro)")
+    
+    
